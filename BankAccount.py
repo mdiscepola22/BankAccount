a68be8815d7c40acd25bf49c0f3c28fbc265fb0d
mdiscepola22 <42971339+mdiscepola22@users.noreply.github.com>
2018-10-16 10:22:34 -0400

    Add files via upload

---
+++ BankAccount.py
@@ -0,0 +1,24 @@
+'''
+Created on Oct 16, 2018
+
+@author: Matt Discepola
+Project: Bank Account
+'''
+
+
+oneName = input("Please enter the account holder's name: ")
+oneNumber = input("Please enter the first account number: ")
+oneType = input("Please enter the type of account: ")
+oneBalance = float(input("Please enter the balance: "))
+twoName = input("Please enter the account holder's name: ")
+twoNumber = input("Please enter the first account number: ")
+twoType = input("Please enter the type of account: ")
+twoBalance = float(input("Please enter the balance: "))
+threeName = input("Please enter the account holder's name: ")
+threeNumber = input("Please enter the first account number: ")
+threeType = input("Please enter the type of account: ")
+threeBalance = float(input("Please enter the balance: "))
+print("")
+print("")
+print("All Accounts:")
+print("Acc Num:", format(oneNumber, '011d'), format("Name:"))
